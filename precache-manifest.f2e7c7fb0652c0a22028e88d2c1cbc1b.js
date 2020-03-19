ed329c2ebf72a114f961cbd2d0a8bf2fadb26f26
Grant Jackson <gej1515@gmail.com>
2020-03-19 15:01:21 -0500

    Updates

---
+++ precache-manifest.f2e7c7fb0652c0a22028e88d2c1cbc1b.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8563a1f8c881635b5b0a32742cd96264",
+    "url": "/portfolio-react/index.html"
+  },
+  {
+    "revision": "e873eacafd4c13630996",
+    "url": "/portfolio-react/static/css/main.4e906039.chunk.css"
+  },
+  {
+    "revision": "efe0440d7cb46d74932c",
+    "url": "/portfolio-react/static/js/2.9fb08241.chunk.js"
+  },
+  {
+    "revision": "87207bc9fa667f722ef4002dcc7edaa4",
+    "url": "/portfolio-react/static/js/2.9fb08241.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e873eacafd4c13630996",
+    "url": "/portfolio-react/static/js/main.27c1cbb4.chunk.js"
+  },
+  {
+    "revision": "38cc46e1fd5c05c02767",
+    "url": "/portfolio-react/static/js/runtime-main.6f440d9c.js"
+  },
+  {
+    "revision": "4d375de2de530d405d2b02f8885b8204",
+    "url": "/portfolio-react/static/media/Scraper.4d375de2.jpg"
+  },
+  {
+    "revision": "7d6ec81a0921a9b1b287cd818cdd40b0",
+    "url": "/portfolio-react/static/media/basil.7d6ec81a.jpg"
+  },
+  {
+    "revision": "188f268f8e888463314b02d584c1e33f",
+    "url": "/portfolio-react/static/media/burger.188f268f.jpg"
+  },
+  {
+    "revision": "ca8c16260748a5e0b64459210e50a05d",
+    "url": "/portfolio-react/static/media/clicky.ca8c1626.jpg"
+  },
+  {
+    "revision": "5f519f71e1643414f907cc6e9b704205",
+    "url": "/portfolio-react/static/media/coffee.5f519f71.jpg"
+  },
+  {
+    "revision": "00ac61bae97832f0993f9b8bc6a8eaa8",
+    "url": "/portfolio-react/static/media/friendfinder.00ac61ba.jpg"
+  },
+  {
+    "revision": "683815ef6cac8557c3ace989ab26a505",
+    "url": "/portfolio-react/static/media/giphy.683815ef.jpg"
+  },
+  {
+    "revision": "72824fd19ce83030b2c0d98f5da52209",
+    "url": "/portfolio-react/static/media/googlebook.72824fd1.jpg"
+  },
+  {
+    "revision": "bbc2b4c57a7b3b5683b8864dc9459bb8",
+    "url": "/portfolio-react/static/media/grant.bbc2b4c5.jpg"
+  },
+  {
+    "revision": "6ebf06f1bc449234f419e105a885a3ce",
+    "url": "/portfolio-react/static/media/hangman.6ebf06f1.jpg"
+  },
+  {
+    "revision": "63f497100bfbdc4598df29f1d0011705",
+    "url": "/portfolio-react/static/media/laterlist.63f49710.jpg"
+  },
+  {
+    "revision": "8b2339c7b071cc521baead48801da8c5",
+    "url": "/portfolio-react/static/media/musicapp.8b2339c7.jpg"
+  },
+  {
+    "revision": "6dcf699938b2291d1634f91adb0dded1",
+    "url": "/portfolio-react/static/media/nodejslogo.6dcf6999.jpg"
+  },
+  {
+    "revision": "30e5b2fe32f384d51c0c38b5203a6b35",
+    "url": "/portfolio-react/static/media/outcache.30e5b2fe.jpg"
+  },
+  {
+    "revision": "4009eef3b3e291f296e71962c70895c5",
+    "url": "/portfolio-react/static/media/react.4009eef3.svg"
+  },
+  {
+    "revision": "109ad6d191c8ebb316b49fe273ba154a",
+    "url": "/portfolio-react/static/media/rpg.109ad6d1.jpg"
+  },
+  {
+    "revision": "2497791151bb5b4de19ab334f1a2e16f",
+    "url": "/portfolio-react/static/media/rps.24977911.jpg"
+  },
+  {
+    "revision": "10c8c9857af8a383351afd7faba23602",
+    "url": "/portfolio-react/static/media/trivia.10c8c985.jpg"
+  },
+  {
+    "revision": "338ec861251e5e5cf0b4f2b75bce8105",
+    "url": "/portfolio-react/static/media/utaustin.338ec861.jpg"
+  }
+]);
